f6495caaa8e3c1c99d277154be9d66b47dfd2411
Hisoka-zak <144565419+Hisoka-zak@users.noreply.github.com>
2024-12-21 17:20:05 +0400

    Update RegisterValidation.js

--- src/validations/RegisterValidation.js
+++ src/validations/RegisterValidation.js
@@ -8,9 +8,17 @@ const ValidationRegisterSchema = Yup.object().shape({
     ProfileUrl:Yup.string().url("Must be valid link !!").notRequired(),
     email:Yup.string().email("Invalid email fromat !!").required("Email is required !!"),
     password:Yup.string().min(8,"Passwore must be at least 8 characters !!")
+        .matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!"
+  )
     .required("Password is required !!"),
     conPassword:Yup.string()
     .oneOf([Yup.ref("password")],'Password must be match !!')
+        .matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!"
+  )
     .required("Password is required !!"),
     checkbox:Yup.bool().oneOf([true],"You must agree all the terms"),
     gender: Yup.string()
